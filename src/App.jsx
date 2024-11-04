a46473d6aa5ecdf268a961e499c0ab4c427f5643
Jayasri24-sri <melpadijayasri2001@gmail.com>
2024-11-04 19:04:52 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import "./App.css";
+import TaskForm from "./components/TaskForm";
+import TaskColumn from "./components/TaskColumn";
+import todoIcon from "./assets/direct-hit.png";
+import doingIcon from "./assets/glowing-star.png";
+import doneIcon from "./assets/check-mark-button.png";
+
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+  console.log("tasks", tasks);
+  return (
+    <div className="app">
+      <TaskForm setTasks={setTasks} />
+      <main className="app_main">
+        <TaskColumn title="To do" icon={todoIcon} tasks={tasks} status="todo" />
+        <TaskColumn
+          title="Doing"
+          icon={doingIcon}
+          tasks={tasks}
+          status="doing"
+        />
+        <TaskColumn title="Done" icon={doneIcon} tasks={tasks} status="done" />
+      </main>
+    </div>
+  );
+};
+
+export default App;
